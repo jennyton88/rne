4a2b89d368ae6f4e234bad5554be598ba42b5bf4
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-09-19 11:38:42 -0400

    backend work

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+// route for api
+app.get("/api", (req, res) => {
+    res.json({"users": ["userOne", "userTwo", "userThree"]}) // backend api
+})
+
+// port 5000 server, port 3000 client
+app.listen(5000, () => { console.log("Server started on port 5000")})
+
+// npm run dev, in package.json where added the start for nodemon
+// localhost:5000/api in browser, see the json data

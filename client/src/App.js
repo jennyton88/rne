b05892d83cfb8315b7936612b155fee41865562a
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-09-19 11:45:12 -0400

    working on backend fetching data and storing it

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 function App() {
+
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then( // relative route since proxy added
+      response => response.json() // give json 
+    ).then(
+      data => {
+        setBackendData(data) // put data into this var
+      }
+    )
+  }, []) // array for running on first render of component
   return (
     <div>
 

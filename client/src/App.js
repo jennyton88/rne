5129a39e37d496102e2bd51cb9804b3e8ff1b713
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-09-19 14:08:00 -0400

    commented out example project code, add testing

--- client/src/App.js
+++ client/src/App.js
@@ -2,28 +2,47 @@ import React, {useEffect, useState} from 'react'
 
 function App() {
 
-  const [backendData, setBackendData] = useState([{}])
+  // const [backendData, setBackendData] = useState([{}])
 
-  useEffect(() => {
-    fetch("/api").then( // relative route since proxy added
-      response => response.json() // give json 
-    ).then(
-      data => {
-        setBackendData(data) // put data into this var
-      }
-    )
-  }, []) // array for running on first render of component
+  // useEffect(() => {
+  //   fetch("/api").then( // relative route since proxy added
+  //     response => response.json() // give json 
+  //   ).then(
+  //     data => {
+  //       setBackendData(data) // put data into this var
+  //     }
+  //   )
+  // }, []) // array for running on first render of component
   return (
     <div>
-      {(typeof backendData.users === 'undefined') ? (
+      {/* {(typeof backendData.users === 'undefined') ? (
         <p>loading...</p>
       ) : (
         backendData.users.map((user, i) => (
           <p key ={i}>{user}</p>
         ))
-      )}
+      )} */}
+
+      <UserPost />
     </div>
   )
+}
+
+function UserPost() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <td>Heading</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Text area</td>
+          </tr>
+        </tbody>
+      </table>
+    )
 }
 
 export default App
--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
-// route for api
-app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThree", "userFour"]}) // backend api
-})
+// // route for api
+// app.get("/api", (req, res) => {
+//     res.json({"users": ["userOne", "userTwo", "userThree", "userFour"]}) // backend api
+// })
 
-// port 5000 server, port 3000 client
-app.listen(5000, () => { console.log("Server started on port 5000")})
+// // port 5000 server, port 3000 client
+// app.listen(5000, () => { console.log("Server started on port 5000")})
 
-// npm run dev, in package.json where added the start for nodemon
-// localhost:5000/api in browser, see the json data
+// // npm run dev, in package.json where added the start for nodemon
+// // localhost:5000/api in browser, see the json data
